b9892f65e6b5cb231814a947fd7bdbffaa82d44b
Tom <42177014+tomShoutTheSecond@users.noreply.github.com>
2024-05-02 23:54:39 +0100

    Update .net version

---
+++ IdeaAnchor/ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace IdeaAnchor.ViewModels
+{
+	public class SubscriptionViewModel
+	{
+		public bool IsBusy { get; set; }
+
+		public SubscriptionViewModel()
+		{
+		}
+	}
+}
+
